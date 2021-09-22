c2752f0d2d5d45719d1a4a870bbdd1d8246fec9f
Enzo Herrera <enzoherrera123@gmail.com>
2021-09-22 15:09:26 -0700

    Initial Commit

---
+++ drinkd-iOS/Model/YelpApi-BusinessDetails.swift
@@ -0,0 +1,73 @@
+//
+//  YelpApi-BusinessDetails.swift
+//  drinkd-iOS
+//
+//  Created by Enzo Herrera on 9/22/21.
+//
+
+import SwiftUI
+
+struct YelpApiBusinessDetails: Codable {
+	let id: String
+	let alias: String
+	let name: String
+	let image_url: String
+	let is_claimed: Bool
+	let is_closed: Bool
+	let url: String
+	let phone: String
+	let display_phone: String
+	let review_count: Int
+	let categories: [YelpApiBusinessDetails_Categories]
+	let rating: Double
+	let location: YelpApiBusinessDetails_Location
+	let coordinates: YelpApiBusinessDetails_Coordinates
+	let photos: [String]
+	let price: String
+	let hours: [YelpApiBusinessDetails_Hours]
+	let transactions: [String]
+	let special_hours: [YelpApiBusinessDetails_SpecialHours]
+}
+
+struct YelpApiBusinessDetails_Categories: Codable {
+	let alias: String
+	let title: String
+}
+
+struct YelpApiBusinessDetails_Location: Codable {
+	let address1: String
+	let address2: String
+	let address3: String
+	let city: String
+	let zip_code: String
+	let country: String
+	let state: String
+	let display_address: [String]
+	let cross_streets: String
+}
+
+struct YelpApiBusinessDetails_Coordinates: Codable {
+	let latitude: Double
+	let longitude: Double
+}
+
+struct YelpApiBusinessDetails_Hours: Codable {
+	let open: [YelpApiBusinessDetails_Hours_Open]
+	let hours_type: String
+	let is_open_now: Bool
+}
+
+struct YelpApiBusinessDetails_Hours_Open: Codable {
+	let is_overnight: Bool
+	let start: String
+	let end: String
+	let day: Int
+}
+
+struct YelpApiBusinessDetails_SpecialHours: Codable {
+	let date: String
+	let is_closed: Bool?
+	let start: String
+	let end: String
+	let is_overnight: Bool
+}
---
+++ drinkd-iOS/drinkd_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  drinkd_iOSApp.swift
+//  drinkd-iOS
+//
+//  Created by Enzo Herrera on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct drinkd_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
