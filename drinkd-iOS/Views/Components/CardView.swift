f7b6f943541c618ba7dcb5772563f9434ac04a76
Enzo Herrera <enzoherrera123@gmail.com>
2021-09-22 20:31:39 -0700

    fixed card alignment

--- drinkd-iOS/Views/Components/CardView.swift
+++ drinkd-iOS/Views/Components/CardView.swift
@@ -9,35 +9,44 @@ import SwiftUI
 
 struct CardView: View {
 	var body: some View {
-		ZStack{
-			VStack {
-				Text("RestaurantTitle")
-					.font(.largeTitle)
-				Text("Restaurant Detail")
-					.font(.title2)
-				Text("Score / Money")
-					.font(.title3)
-				Image("drinkd_text")
-					.resizable()
-					.scaledToFit()
-				Group {
-					HStack {
-						Image(systemName: "house")
-							.resizable()
-							.scaledToFit()
-							.frame(width: 40)
-						Text("Address")
+
+		GeometryReader{ geo in
+			ZStack{
+				VStack {
+					Text("RestaurantTitle")
+						.font(.largeTitle)
+					Text("Restaurant Detail")
+						.font(.title2)
+					Text("Score / Money")
+						.font(.title3)
+					Image("drinkd_text")
+						.resizable()
+						.scaledToFit()
+						.frame(width: geo.frame(in: .global).width)
+					Group {
+						HStack {
+							Image(systemName: "house")
+								.resizable()
+								.scaledToFit()
+								.frame(width: 40)
+							Text("Address")
+						}
+						HStack {
+							Image(systemName: "phone")
+								.resizable()
+								.scaledToFit()
+								.frame(width: 40)
+							Text("Phone number")
+						}
 					}
-					HStack {
-						Image(systemName: "phone")
-							.resizable()
-							.scaledToFit()
-							.frame(width: 40)
-						Text("Phone number")
+					Button(action: {}) {
+						Text("Sign In")
 					}
+					.background(Color.purple)
 				}
 			}
 		}
+
 	}
 }
 
