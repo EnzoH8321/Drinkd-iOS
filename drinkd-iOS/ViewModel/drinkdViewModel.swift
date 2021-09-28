0c871c8df2952e6f8af1d8da44e0e7eded930b87
Enzo Herrera <enzoherrera123@gmail.com>
2021-09-27 17:02:05 -0700

    added view model and yelp url

---
+++ drinkd-iOS/ViewModel/drinkdViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  drinkdViewModel.swift
+//  drinkd-iOS
+//
+//  Created by Enzo Herrera on 9/27/21.
+//
+
+import Foundation
+
+class drinkdViewModel {
+
+
+
+}
+
---
+++ drinkd-iOS/Views/SplashScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreen.swift
+//  drinkd-iOS
+//
+//  Created by Enzo Herrera on 9/27/21.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text("Splash Screen")
+    }
+}
+
+struct SplashScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreen()
+    }
+}
