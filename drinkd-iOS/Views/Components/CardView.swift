e77f0f13af538a05630e929590f1f458a691609e
Enzo Herrera <enzoherrera123@gmail.com>
2021-09-29 13:15:30 -0700

    Fixed transaction array of api - destructures values

--- drinkd-iOS/Views/Components/CardView.swift
+++ drinkd-iOS/Views/Components/CardView.swift
@@ -18,6 +18,13 @@ struct CardView: View {
 		case mediumPadding = 12
 	}
 
+	private enum TransactionTypes: String {
+		case pickup
+		case delivery
+		case restaurant_reservation
+
+	}
+
 	var restaurantTitle: String
 	var restaurantCategories: String
 	var restaurantScore: Int
@@ -30,7 +37,9 @@ struct CardView: View {
 	var restaurantZipCode: String
 	var restaurantState: String
 	var restaurantURL: String
-	var restaurantPurchaseOptions: [String] 
+	var optionsDelivery: Bool = false
+	var optionsPickup: Bool = false
+	var optionsReservations: Bool = false
 
 
 	//We use optionals because the API can return null for some properties
@@ -47,7 +56,25 @@ struct CardView: View {
 		self.restaurantZipCode = restaurantDetails.location?.zip_code ?? ""
 		self.restaurantState = restaurantDetails.location?.state ?? ""
 		self.restaurantURL = restaurantDetails.url ?? ""
-		self.restaurantPurchaseOptions = restaurantDetails.transactions ?? [""]
+
+		let restaurantArray = restaurantDetails.transactions ?? [""]
+
+		for element in restaurantArray {
+			switch(element) {
+			case TransactionTypes.pickup.rawValue:
+				optionsPickup = true
+
+			case TransactionTypes.delivery.rawValue:
+				optionsDelivery = true
+
+			case TransactionTypes.restaurant_reservation.rawValue:
+				optionsReservations = true
+
+			default:
+				print("Nothing Found")
+			}
+		}
+
 	}
 
 	var body: some View {
@@ -69,8 +96,8 @@ struct CardView: View {
 					Text("\(restaurantScore) / \(restaurantPrice)")
 						.font(.title3)
 					RemoteImageLoader(url: "\(restaurantImage)")
-//						.scaledToFit()
-//						.frame(width: localWidth, alignment: .center)
+					//						.scaledToFit()
+					//						.frame(width: localWidth, alignment: .center)
 					Group {
 						HStack {
 							Image(systemName: "house")
@@ -87,6 +114,34 @@ struct CardView: View {
 								.frame(width: 40)
 							Text("\(restaurantPhoneNumber)")
 						}
+						if (optionsPickup) {
+							HStack {
+								Image(systemName: "figure.walk")
+									.resizable()
+									.scaledToFit()
+									.frame(width: 40)
+								Text("Pickup Available")
+							}
+						}
+						if (optionsDelivery) {
+							HStack {
+								Image(systemName: "bicycle")
+									.resizable()
+									.scaledToFit()
+									.frame(width: 40)
+								Text("Delivery Available")
+							}
+						}
+						if (optionsReservations) {
+							HStack {
+								Image(systemName: "square.and.pencil")
+									.resizable()
+									.scaledToFit()
+									.frame(width: 40)
+								Text("Reservations Available")
+							}
+						}
+
 					}
 					HStack {
 						Spacer()
@@ -105,6 +160,6 @@ struct CardView: View {
 struct CardView_Previews: PreviewProvider {
 
 	static var previews: some View {
-		CardView(in: YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 56, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["none"], price: "454", location: YelpApiBusinessDetails_Location(address1: "4545", address2: "4545", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "test", display_phone: "test", distance: 6565.56))
+		CardView(in: YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 56, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery, pickup"], price: "454", location: YelpApiBusinessDetails_Location(address1: "4545", address2: "4545", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "test", display_phone: "test", distance: 6565.56))
 	}
 }
