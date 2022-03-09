1a75e5cffb65a3a19353ba54eacd0c8b335f949c
Enzo Herrera <enzoherrera123@gmail.com>
2022-03-09 11:00:43 -0800

    Initiated ViewModel testing

---
+++ drinkd-iOSTests/ViewModel_Methods_Tests.swift
@@ -0,0 +1,59 @@
+//
+//  ViewModel_Methods_Tests.swift
+//  drinkd-iOSTests
+//
+//  Created by Enzo Herrera on 3/8/22.
+//
+
+import XCTest
+@testable import drinkd_iOS
+
+class ViewModel_Methods_Tests: XCTestCase {
+    
+    var sut: drinkdViewModel!
+    var model: drinkdModel!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = drinkdViewModel()
+        model = drinkdModel()
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        sut = nil
+        model = nil
+    }
+    
+    private func addLocalRestaurants() {
+        guard let data = try? getData(fromJSON: "MockYelpBusinessSearchProperties") else {
+            XCTFail("Could not get data")
+            return
+        }
+        guard let decode = try? JSONDecoder().decode(YelpApiBusinessSearchProperties.self, from: data) else {
+            XCTFail("Could not decode")
+            return
+        }
+        
+        sut.model.appendDeliveryOptions(in: [decode, decode, decode, decode, decode, decode, decode, decode, decode])
+    }
+    
+    //Start
+    func test_updateRestaurantList() throws {
+        sut.updateRestaurantList()
+        sut.updateRestaurantList()
+        
+        XCTAssertEqual(sut.model.counter, 7)
+    }
+    
+    func test_createNewParty() throws {
+        sut.createNewParty(setVotes: 5, setName: "Enzo")
+        
+        XCTAssertEqual(sut.partyMaxVotes, 5)
+        XCTAssertEqual(sut.partyName, "Enzo")
+        XCTAssertEqual(sut.currentlyInParty, true)
+    }
+    
+}
