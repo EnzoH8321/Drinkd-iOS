c9d68ae5a840a3d90f1ab1205c470ae0dc04dc9c
EnzoHerrera <enzoherrera@cattlekrush.com>
2025-04-30 15:05:27 -0700

    Adds platform list to package.swift

--- drinkdSharedModels/Package.swift
+++ drinkdSharedModels/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "drinkdSharedModels",
+    platforms: [
+        .macOS(.v15),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
