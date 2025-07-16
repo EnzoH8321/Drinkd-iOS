a8f286313fe35b1cd753a661a60fa6e9f8c83f40
EnzoHerrera <enzoherrera@cattlekrush.com>
2025-07-16 16:01:40 -0700

    removes persistence controller code

--- drinkd-iOS/drinkd_iOSApp.swift
+++ drinkd-iOS/drinkd_iOSApp.swift
@@ -43,16 +43,14 @@ struct TestApp: App {
 
 struct drinkd_iOSApp: App {
 
-	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
-	let persistenceController = PersistenceController.shared
+//	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
 	@State var viewModel = PartyViewModel()
 
 	var body: some Scene {
 		WindowGroup {
 			MasterView()
-				.environment(\.managedObjectContext, persistenceController.container.viewContext)
+//				.environment(\.managedObjectContext, persistenceController.container.viewContext)
 				.environment(viewModel)
 				.onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
 
@@ -86,77 +84,46 @@ struct drinkd_iOSApp: App {
 	}
 }
 
-//The first one is executed once the app finished launching. We are configuring Firebase, then asking the user for permission to send them push notifications.
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, ObservableObject {
-	let gcmMessageIDKey = "gcm.message_id"
-	static var fcmToken: String = "TestTOKEN"
-
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
-		if #available(iOS 10.0, *) {
-			//FOR IOS 10 DISPLAY NOTIFICATION
-			UNUserNotificationCenter.current().delegate = self
-			let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-			UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_,_ in})
-		} else {
-			let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-			application.registerUserNotificationSettings(settings)
-		}
-
-		application.registerForRemoteNotifications()
-		return true
-	}
-
-	//The second function listens to remote notifications and will alert the app when a new push notification comes in.
-	func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-					 fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-
-		if let messageID = userInfo[gcmMessageIDKey] {
-			print("Message ID: \(messageID)")
-		}
-
-		completionHandler(UIBackgroundFetchResult.newData)
-	}
-}
 
 
+//TODO: Maybe get back to this, old code for push notifications.
 // It's a UNUserNotificationCenterDelegate and you can think of it as a Notification Center. This is where all the notification actions are handled.
-@available(iOS 10, *)
-extension AppDelegate {
-
-	// Receive displayed notifications for iOS 10 devices.
-	func userNotificationCenter(_ center: UNUserNotificationCenter,
-								willPresent notification: UNNotification,
-								withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-
-		let userInfo = notification.request.content.userInfo
-
-		if let messageID = userInfo[gcmMessageIDKey] {
-			print("Message ID: \(messageID)")
-		}
-
-		// Change this to your preferred presentation option
-		completionHandler([[.banner, .badge, .sound]])
-	}
-
-	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-
-	}
-
-	func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-
-	}
-
-	func userNotificationCenter(_ center: UNUserNotificationCenter,
-								didReceive response: UNNotificationResponse,
-								withCompletionHandler completionHandler: @escaping () -> Void) {
-
-		let userInfo = response.notification.request.content.userInfo
-
-		if let messageID = userInfo[gcmMessageIDKey] {
-			print("Message ID from userNotificationCenter didReceive: \(messageID)")
-		}
-
-		completionHandler()
-	}
-}
+//@available(iOS 10, *)
+//extension AppDelegate {
+//
+//	// Receive displayed notifications for iOS 10 devices.
+//	func userNotificationCenter(_ center: UNUserNotificationCenter,
+//								willPresent notification: UNNotification,
+//								withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//
+//		let userInfo = notification.request.content.userInfo
+//
+//		if let messageID = userInfo[gcmMessageIDKey] {
+//			print("Message ID: \(messageID)")
+//		}
+//
+//		// Change this to your preferred presentation option
+//		completionHandler([[.banner, .badge, .sound]])
+//	}
+//
+//	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//
+//	}
+//
+//	func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+//
+//	}
+//
+//	func userNotificationCenter(_ center: UNUserNotificationCenter,
+//								didReceive response: UNNotificationResponse,
+//								withCompletionHandler completionHandler: @escaping () -> Void) {
+//
+//		let userInfo = response.notification.request.content.userInfo
+//
+//		if let messageID = userInfo[gcmMessageIDKey] {
+//			print("Message ID from userNotificationCenter didReceive: \(messageID)")
+//		}
+//
+//		completionHandler()
+//	}
+//}
