dbff9ca4f503ce5b463a3633efcef67587884110
EnzoHerrera <enzoherrera@cattlekrush.com>
2025-08-04 17:30:12 -0700

    Fixes log

--- drinkdSharedModels/Sources/drinkdSharedModels/Log.swift
+++ drinkdSharedModels/Sources/drinkdSharedModels/Log.swift
@@ -11,20 +11,20 @@ import Logging
 
 public final class Log {
     /// Using your bundle identifier is a great way to ensure a unique identifier.
-    private static let subsystem = Bundle.main.bundleIdentifier
+    private static let subsystem = Bundle.main.bundleIdentifier ?? "drinkdVaporServer"
 
 //    public static let supabase = Logger(subsystem: subsystem ?? "Error locating Subsystem", category: "Supabase")
-    public static let supabase = Logger(label: "\(subsystem ?? "Error locating Subsystem") category: Supabase")
+    public static let supabase = Logger(label: "\(subsystem) category: Supabase")
 
 //    public static let networking = Logger(subsystem: subsystem ?? "Error locating Subsystem", category: "Networking")
-    public static let networking = Logger(label: "\(subsystem ?? "Error locating Subsystem") category: Networking")
+    public static let networking = Logger(label: "\(subsystem) category: Networking")
 
 //    public static let routes = Logger(subsystem: subsystem ?? "Error locating Subsystem", category: "Routes")
-    public static let routes = Logger(label: "\(subsystem ?? "Error locating Subsystem") category: Routes")
+    public static let routes = Logger(label: "\(subsystem) category: Routes")
 
 //    public static let userDefaults = Logger(subsystem: subsystem ?? "Error locating Subsystem", category: "User Defaults")
-    public static let userDefaults = Logger(label: "\(subsystem ?? "Error locating Subsystem") category: User Defaults")
+    public static let userDefaults = Logger(label: "\(subsystem) category: User Defaults")
 
 //    public static let general = Logger(subsystem: subsystem ?? "Error locating Subsystem", category: "General")
-    public static let general = Logger(label: "\(subsystem ?? "Error locating Subsystem") category: General")
+    public static let general = Logger(label: "\(subsystem) category: General")
 }
