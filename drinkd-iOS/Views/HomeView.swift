cb504cd52d65157838f3ec65b00a1303b8d23b06
EnzoHerrera <enzoherrera@cattlekrush.com>
2025-10-03 09:10:00 -0700

    Removes obfuscation library. Removes API key references.

--- drinkd-iOS/Views/HomeView.swift
+++ drinkd-iOS/Views/HomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import drinkdSharedModels
 
 struct HomeView: View {
     @Environment(PartyViewModel.self) var viewModel
@@ -47,32 +48,32 @@ extension HomeView {
 }
 
 
-#Preview("In a Party") {
-    let partyVM = PartyViewModel()
-    let party = Party(username: "USERNAME01" ,partyID: UUID(uuidString: "6f31b771-0027-4407-8c97-07a7609d3e2b")!, partyMaxVotes: 1, partyName: "Party Name", partyCode: 123123 ,yelpURL: "YELP API ")
-    partyVM.currentParty = party
-
-    let searchProp1 = YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds1", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 5, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery", "pickup"], price: "$$", location: YelpApiBusinessDetails_Location(address1: "155 W 51st St", address2: "Suite 1-", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "650-339-0869", display_phone: "test", distance: 6565.56)
-
-    let searchProp2 = YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds2", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 5, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery", "pickup"], price: "$$", location: YelpApiBusinessDetails_Location(address1: "155 W 51st St", address2: "Suite 1-", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "650-339-0869", display_phone: "test", distance: 6565.56)
-
-    let searchProp3 = YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds3", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 5, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery", "pickup"], price: "$$", location: YelpApiBusinessDetails_Location(address1: "155 W 51st St", address2: "Suite 1-", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "650-339-0869", display_phone: "test", distance: 6565.56)
-
-
-    partyVM.localRestaurants.append(contentsOf: [
-        searchProp1,
-        searchProp2,
-        searchProp3,
-        searchProp1,
-        searchProp2,
-        searchProp3,
-        searchProp1,
-        searchProp2,
-        searchProp3
-    ]
-    )
-
-    return HomeView()
-        .environment(partyVM)
-        .environment(Networking())
-}
+//#Preview("In a Party") {
+//    let partyVM = PartyViewModel()
+//    let party = Party(username: "USERNAME01" ,partyID: UUID(uuidString: "6f31b771-0027-4407-8c97-07a7609d3e2b")!, partyMaxVotes: 1, partyName: "Party Name", partyCode: 123123 ,yelpURL: "YELP API ")
+//    partyVM.currentParty = party
+//
+//    let searchProp1 = YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds1", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 5, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery", "pickup"], price: "$$", location: YelpApiBusinessDetails_Location(address1: "155 W 51st St", address2: "Suite 1-", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "650-339-0869", display_phone: "test", distance: 6565.56)
+//
+//    let searchProp2 = YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds2", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 5, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery", "pickup"], price: "$$", location: YelpApiBusinessDetails_Location(address1: "155 W 51st St", address2: "Suite 1-", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "650-339-0869", display_phone: "test", distance: 6565.56)
+//
+//    let searchProp3 = YelpApiBusinessSearchProperties(id: "43543", alias: "harvey", name: "Mcdonalds3", image_url: "", is_closed: true, url: "", review_count: 7, categories: [YelpApiBusinessDetails_Categories(alias: "test", title: "Bars")], rating: 5, coordinates: YelpApiBusinessDetails_Coordinates(latitude: 565.5, longitude: 45.5), transactions: ["delivery", "pickup"], price: "$$", location: YelpApiBusinessDetails_Location(address1: "155 W 51st St", address2: "Suite 1-", address3: "34343", city: "san carlos", zip_code: "454545", country: "america", state: "cali", display_address: ["test this"], cross_streets: "none"), phone: "650-339-0869", display_phone: "test", distance: 6565.56)
+//
+//
+//    partyVM.localRestaurants.append(contentsOf: [
+//        searchProp1,
+//        searchProp2,
+//        searchProp3,
+//        searchProp1,
+//        searchProp2,
+//        searchProp3,
+//        searchProp1,
+//        searchProp2,
+//        searchProp3
+//    ]
+//    )
+//
+//    return HomeView()
+//        .environment(partyVM)
+//        .environment(Networking())
+//}
